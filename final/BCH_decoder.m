f005c910033c32fced6f9c6d0dac7a7ad1da4121
chinnichaitanya <chchaitanya95@gmail.com>
2017-03-28 23:56:30 +0530

    Modify the variable names in decoder

--- final/BCH_decoder.m
+++ final/BCH_decoder.m
@@ -58,83 +58,83 @@ for rx_msg = rx_messages
         % convert those integer represented numbers to GF-elements %
         bch_roots = gf(integer_representation, M, PRIM_POLY);
         % calculate the syndrome at the roots %
-        s = polyval(Rx_poly, bch_roots);
+        syndrome = polyval(Rx_poly, bch_roots);
         
         % simplified iterative algorithm for finding error location poly
-        u = [-0.5, 0, 1, 2, 3, 4, 5, 6, 7];
-        d = gf([1, 0, 0, 0, 0, 0, 0, 0, 0], M, PRIM_POLY);
-        l = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        diff = [-1, 0, 0, 0, 0, 0, 0, 0, 0];
+        Mu = [-0.5, 0, 1, 2, 3, 4, 5, 6, 7];
+        Discrepancy = gf([1, 0, 0, 0, 0, 0, 0, 0, 0], M, PRIM_POLY);
+        L = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        Diff = [-1, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        a = zeros(9, 84);
-        a(:,1) = 1;
+        sigma_dec = zeros(9, 84);
+        sigma_dec(:,1) = 1;
 
-        A = gf(a, M, PRIM_POLY);
-        A(3,2) = s(1);
+        Sigma = gf(sigma_dec, M, PRIM_POLY);
+        Sigma(3, 2) = syndrome(1);
         
-        d(2) = s(1);
+        Discrepancy(2) = syndrome(1);
         
         for i = 1:6
           j = 84;
-          while A(i+2,j) == 0
+          while Sigma(i+2,j) == 0
               j = j-1;
           end
-          l(i+2) = j - 1;
-          diff(i+2) = 2*u(i+2) - l(i+2);
-          d(i+2) = 0;
-          for k = 0:l(i+2) 
-              d(i+2)= d(i+2) + A(i+2, k+1)*s(2*i+1-k);
+          L(i+2) = j - 1;
+          Diff(i+2) = 2*Mu(i+2) - L(i+2);
+          Discrepancy(i+2) = 0;
+          for k = 0:L(i+2) 
+              Discrepancy(i+2)= Discrepancy(i+2) + Sigma(i+2, k+1)*syndrome(2*i+1-k);
           end
           
-          if d(i+2) == 0
-              A(i+3, :) = A(i+2, :);
+          if Discrepancy(i+2) == 0
+              Sigma(i+3, :) = Sigma(i+2, :);
           else 
-              max = diff(2);
-              v = 2;
+              max = Diff(2);
+              rho_index = 2;
               for t = 2:(i+1)
-                  if d(t) ~= 0
-                     if max < diff(t)
-                         max = diff(t);
-                         v = t;
+                  if Discrepancy(t) ~= 0
+                     if max < Diff(t)
+                         max = Diff(t);
+                         rho_index = t;
                      end
                   end 
               end
-              b = d(i+2)/d(v);
-              c = 2*(u(i+2) - u(v));
-       
-              w = mul_poly(c, A(v, :), l(v), M, PRIM_POLY);
-              A(i+3, :) = A(i+2, :) + b*w;
+              
+              coeff = Discrepancy(i+2)/Discrepancy(rho_index);
+              degree = 2*(Mu(i+2) - Mu(rho_index));
+              add_poly = mul_poly(degree, Sigma(rho_index, :), L(rho_index), M, PRIM_POLY);
+              Sigma(i+3, :) = Sigma(i+2, :) + coeff*add_poly;
           end
         end
         
         j = 84;
-        while A(9, j) == 0
+        while Sigma(9, j) == 0
           j = j-1;
         end
         if j > 8
             % break 
         else 
             % correcting each bit
-            err_poly = gf(fliplr(A(9, :)), M, PRIM_POLY);
+            err_poly = gf(fliplr(Sigma(9, :)), M, PRIM_POLY);
             int_rep_all = bi2de(FIELD(2:128, :), P, 'right-msb');
             ele_field = gf(int_rep_all, M, PRIM_POLY);
             err_vector = polyval(err_poly, ele_field);
 
-            corr_vector = gf(zeros(1, 127), M, PRIM_POLY);
+            est_codeword = gf(zeros(1, 127), M, PRIM_POLY);
             for h = 2:127
                 if err_vector(h) == 0
-                    corr_vector(h-1) = 1 + Rx_poly(h-1);
+                    est_codeword(h-1) = 1 + Rx_poly(h-1);
                 else
-                    corr_vector(h-1) = Rx_poly(h-1); 
+                    est_codeword(h-1) = Rx_poly(h-1); 
                 end
             end
             if err_vector(1) == 0
-               corr_vector(127) = 1 + Rx_poly(127); 
+               est_codeword(127) = 1 + Rx_poly(127); 
             else
-               corr_vector(127) = Rx_poly(127); 
+               est_codeword(127) = Rx_poly(127); 
             end
 
-            corr_vector = fliplr(corr_vector);
+            est_codeword = fliplr(est_codeword);
         end 
     end
 end
